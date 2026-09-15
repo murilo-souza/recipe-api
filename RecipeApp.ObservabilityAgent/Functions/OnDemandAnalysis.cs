975ff5a68b653f89a7ae2599b42cacf6a5c39e97
Murilo Souza <murilo.l.souza232@gmail.com>
2026-09-14 22:12:42 -0300

    feat: create folder function and remove useless function

--- RecipeApp.ObservabilityAgent/Functions/OnDemandAnalysis.cs
+++ RecipeApp.ObservabilityAgent/Functions/OnDemandAnalysis.cs
@@ -3,18 +3,18 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 
-namespace RecipeApp.ObservabilityAgent;
+namespace RecipeApp.ObservabilityAgent.Functions;
 
-public class Function1
+public class OnDemandAnalysis
 {
-    private readonly ILogger<Function1> _logger;
+    private readonly ILogger<OnDemandAnalysis> _logger;
 
-    public Function1(ILogger<Function1> logger)
+    public OnDemandAnalysis(ILogger<OnDemandAnalysis> logger)
     {
         _logger = logger;
     }
 
-    [Function("Function1")]
+    [Function("OnDemandAnalysis")]
     public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
     {
         _logger.LogInformation("C# HTTP trigger function processed a request.");
--- RecipeApp.ObservabilityAgent/Functions/ScheduledAnalysis.cs
+++ RecipeApp.ObservabilityAgent/Functions/ScheduledAnalysis.cs
@@ -2,18 +2,18 @@ using System;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 
-namespace RecipeApp.ObservabilityAgent;
+namespace RecipeApp.ObservabilityAgent.Functions;
 
-public class Function
+public class ScheduledAnalysis
 {
     private readonly ILogger _logger;
 
-    public Function(ILoggerFactory loggerFactory)
+    public ScheduledAnalysis(ILoggerFactory loggerFactory)
     {
-        _logger = loggerFactory.CreateLogger<Function>();
+        _logger = loggerFactory.CreateLogger<ScheduledAnalysis>();
     }
 
-    [Function("Function")]
+    [Function("ScheduledAnalysis")]
     public void Run([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer)
     {
         _logger.LogInformation("C# Timer trigger function executed at: {executionTime}", DateTime.Now);
